26e719ae16f33d14869c0d21cf282eb84ef8ed85
Cookiecodess <142579195+Cookiecodess@users.noreply.github.com>
2025-03-19 15:34:45 +0800

    initial commit

---
+++ main.asm
@@ -0,0 +1,25 @@
+INCLUDE Irvine32.inc
+
+.data
+	; string	byte	"you: hello assembly :) i hope we become great friends", 0
+	; string2 byte	"me:  hello :) im gonna be ur worst nightmare ;)", 0""
+.code
+main PROC
+	mov eax, 1
+	mov ecx, 0
+	
+	; l1:
+		; inc eax
+		; call dumpregs
+		; loop l1
+	; lea edx,string
+	; call WriteString
+	; call CrLf
+
+	; lea edx, string2
+	; call WriteString
+
+	exit
+main ENDP
+END main
+

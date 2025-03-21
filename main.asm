aed5e18cd47a7d8a27c9f20dfe7e529cdd4abf68
du-52 <songlynn12@gmail.com>
2025-03-21 15:18:54 +0800

    user

--- main.asm
+++ main.asm
@@ -12,15 +12,28 @@ INCLUDE Irvine32.inc
 
 	;RECEIPT--------------------------------
 		receipt byte "Receipt",0
-	
+		
+		person	byte ?
+		personS	byte "Name : ",0
+		total	byte ?
+		totalS	byte "Total : ",0
+		price	byte ?
+		priceS	byte "Price : ",0
+		detail	byte ?
+		detailS byte "Details : ",0
+		bookingS	byte "Your booking number is : ",0
+		bookingnum	dword ?
 	;REPORT--------------------------------------------------------------------
+		totalprice byte ?
+		totalticketsold byte ?
 
 		report byte "Report",0
 
 .code
 main PROC
+
 	;USER LOGIN------------------------------
-		
+		;LOGIN:
 		lea edx,header
 
 		call WriteString
@@ -49,18 +62,41 @@ main PROC
 		call WaitMsg
 
 		call Clrscr
+
+		;JMP LOGIN
 	;RECEIPT--------------------------------
+		lea edx,header
+		call WriteString
 		lea edx,receipt
+		call WriteString
+		lea edx,header
+		call WriteString
+		call CRLF
+		
+		lea edx,bookingS
+		call WriteString
+		mov  eax,yellow+(blue*16)
+      call SetTextColor
+		;call Random32
+		call Randomize
+		mov eax, 90000000			;since rand is from 0 to n-1 so it will generate 0 - 89999999
+		call RandomRange		; EAX = random number in range 0-89999999
+		add eax, 10000000			; adjust to range 10000000-99999999
+		mov bookingnum,eax
+
+		call WriteDec
+		call CRLF
+
 		
 
 
 
-
+		call CRLF
 		mov  eax,1000 ;delay 1 sec
 		call Delay
 
 	;REPORT---------------------------------
-		
+		;ticket price * ticket sold
 		
 		lea edx,header
 		call WriteString

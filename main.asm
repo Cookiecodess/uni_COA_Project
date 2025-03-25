bccb24a85df4a4968f4a57073ec6eb15e8a80f8e
Cookiecodess <142579195+Cookiecodess@users.noreply.github.com>
2025-03-25 09:23:21 +0800

    Add dynamic header generation function

--- main.asm
+++ main.asm
@@ -3,7 +3,14 @@ INCLUDE Irvine32.inc
 	LF = 0Ah	; Line Feed
 	SPACE =09h
 .data
-	header byte	"======================================",0
+	header1             byte "Main Menu",0
+    headerLogin			byte "Login",0
+    headerReceipt		byte "Receipt",0
+    header3             byte "Register",0
+    equalSign           byte '='
+    leftRightPadding    dword 10
+
+	; header byte	"======================================",0
 	;USER LOGIN----------------------------------------------------------------
 		welcome byte " Welcome,here is a login page",0
 		choose byte "Choose a number to login",0
@@ -36,14 +43,17 @@ main PROC
 
 	;USER LOGIN------------------------------
 		LOGIN:
-		lea edx,header
+		mov eax, offset headerLogin
+		mov ebx, lengthof headerLogin
+		call PrintHeader
+		; lea edx,header
 
-		call WriteString
-		lea edx,welcome
-		call WriteString
-		lea edx,header
-		call WriteString
-		call CRLF
+		; call WriteString
+		; lea edx,welcome
+		; call WriteString
+		; lea edx,header
+		; call WriteString
+		; call CRLF
 	
 		lea edx,choose
 		call WriteString
@@ -67,13 +77,16 @@ main PROC
 
 		;JMP LOGIN
 	;RECEIPT--------------------------------
-		lea edx,header
-		call WriteString
-		lea edx,receipt
-		call WriteString
-		lea edx,header
-		call WriteString
-		call CRLF
+		mov eax, offset headerReceipt
+		mov ebx, lengthof headerReceipt
+		call PrintHeader
+		; lea edx,header
+		; call WriteString
+		; lea edx,receipt
+		; call WriteString
+		; lea edx,header
+		; call WriteString
+		; call CRLF
 
 		mov al, SPACE 
 		call WriteChar 
@@ -117,23 +130,27 @@ main PROC
 
 	;REPORT---------------------------------
 		;ticket price * ticket sold
-		
-		lea edx,header
-		call WriteString
-		call WriteString
-		call CRLF				; new line
 
-		mov al, SPACE   ; Load tab character
-		call WriteChar  ; Print tab
-		call WriteChar  ; Print tab
-		call WriteChar  ; Print tab
-		lea edx,report
-		call WriteString
-		call CRLF
 
-		lea edx,header
-		call WriteString
-		call WriteString
+		mov eax, offset report
+		mov ebx, lengthof report
+		call PrintHeader
+		; lea edx,header
+		; call WriteString
+		; call WriteString
+		; call CRLF				; new line
+
+		; mov al, SPACE   ; Load tab character
+		; call WriteChar  ; Print tab
+		; call WriteChar  ; Print tab
+		; call WriteChar  ; Print tab
+		; lea edx,report
+		; call WriteString
+		; call CRLF
+
+		; lea edx,header
+		; call WriteString
+		; call WriteString
 
 
 		;call WaitMsg
@@ -141,4 +158,84 @@ main PROC
 		;call LOGIN
 	exit
 main ENDP
+
+;--------------------------------------------------------
+; PrintHeader
+;
+; Description:
+;   Prints a formatted header with top and bottom borders of `=` characters.
+;
+; Parameters:
+;   EAX  - (DWORD) Address of the header string (offset)
+;   EBX  - (DWORD) Length of the header string
+;
+; Returns:
+;   None
+;
+; Example usage:
+;   mov eax, offset header
+;   mov ebx, lengthof header
+;   call PrintHeader
+;--------------------------------------------------------
+PrintHeader proc
+    push ebp        ; save old base pointer
+    mov ebp, esp    ; store the base address of the stack frame
+    sub esp, 8      ; allocate memory for local variables
+
+    ; store local variables
+    mov dword ptr [ebp-4], eax
+    mov dword ptr [ebp-8], ebx
+
+    ; print top border
+    ; for (2*leftRightPadding + lengthof(header)) { print('='); }
+    mov eax, leftRightPadding
+    mov ecx, 2
+    mul ecx
+    add eax, dword ptr [ebp-8]  
+    mov ecx, eax
+PrintLoop:
+    mov al, equalSign
+    call WriteChar
+    loop PrintLoop
+
+    ; print newline
+    call Crlf  ; Irvine32 provides this
+
+    ; print left padding spaces
+    mov ecx, leftRightPadding
+SpaceLoop:
+    mov al, ' '
+    call WriteChar
+    loop SpaceLoop
+
+    ; print header text
+    mov edx, [ebp-4]  ; Adjusted stack offset
+    call WriteString
+
+    ; print newline
+    call Crlf
+
+    ; print bottom border
+    ; for (2*leftRightPadding + lengthof(header)) { print('='); }
+    mov eax, leftRightPadding
+    mov ebx, 2
+    mul ebx
+    add eax, [ebp-8]  ; Adjusted
+    mov ecx, eax
+PrintLoop2:
+    mov al, equalSign
+    call WriteChar
+    loop PrintLoop2
+
+    ; print newline
+    call Crlf
+    
+    ; restore stack pointer
+    mov esp, ebp
+    ; restore base pointer
+    pop ebp
+    ret
+PrintHeader endp
+
+
 END main
